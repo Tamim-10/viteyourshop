c3df1d56bd15fa691772fa7f6cfcb970f84f8b89
Md Tamim Arshad <tamimarshad7@gmail.com>
2024-08-06 23:53:53 +0530

    Updated package,json

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import {
 } from "react-router-dom";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/styles/bootstrap.custom.css";
-import "./assets/styles/index.css";
+import "./assets/styles/index.css";  
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./Screens/HomeScreen";
